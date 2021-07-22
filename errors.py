5879cb93bd767bd505718b182d33afd0ec3e1015
gavrilova21 <gavrilova212121@mail.ru>
2021-07-22 14:11:24 +0300

    add new exception

--- errors.py
+++ errors.py
@@ -1,3 +1,8 @@
 class NotFoundYandexMusicException(Exception):
     """Исключение если трек не найден в Я Музыке"""
     pass
+
+
+class NotFoundMusicxException(Exception):
+    """Исключение если трек не найден в Я Музыке"""
+    pass
